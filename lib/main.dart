b4bc20211f62ffc638e080c216d91bc8e10cbd46
Home <dgryzhkov@gmail.com>
2021-10-14 22:36:53 +0700

    manual JSON

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:http_less/offices.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,37 +21,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<OfficesList> officesList;
   @override
   void initState() {
     super.initState();
-    loadData();
+    officesList = getOfficesList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Networking'),
+        title: Text('Manual JSON Serialization'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: FutureBuilder<OfficesList>(
+        future: officesList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.offices.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('${snapshot.data!.offices[index].name}'),
+                      subtitle:
+                          Text('${snapshot.data!.offices[index].address}'),
+                      leading: Image.network(
+                          '${snapshot.data!.offices[index].image}'),
+                      isThreeLine: true,
+                    ),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text('Error');
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
-}
-
-Future<http.Response> getData() async {
-  const url = 'https://about.google/static/data/locations.json';
-  return await http.get(Uri.parse(url));
-}
-
-void loadData() {
-  getData().then((response) {
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      print(response.statusCode);
-    }
-  }).catchError((error) {
-    debugPrint(error.toString());
-  });
 }
